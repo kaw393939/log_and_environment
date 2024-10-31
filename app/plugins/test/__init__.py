7d0b6672bbe5ebe0309fa33472e0c46384d8a8fb
Ria <vg435@njit.edu>
2024-10-31 17:38:12 -0400

    icecream debug log

--- app/plugins/test/__init__.py
+++ app/plugins/test/__init__.py
@@ -1,8 +1,13 @@
 import logging
+from icecream import ic
 from app.commands import Command
 
 class TestCommand(Command):
     # inheriting from abc command
     def execute(self):
         logging.info("This is a test message!")
+
+        mydict = {1,2,3,4,5}
+        logging.debug(print(ic(mydict)))
+
         print("This is a test message!")
